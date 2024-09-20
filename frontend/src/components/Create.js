77f51f0d4a31564e334f424505eeea13e588ac95
KristaSmith247 <kristasmith247@gmail.com>
2024-09-19 20:57:01 -0600

    Updated login and create account

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -1,92 +1,172 @@
-import { React, useState, useEffect } from "react";
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
-export default function Create() {
-	const [form, setForm] = useState({
-		// default state of account creation form
-		username: "",
-		password: "",
-		passwordConfirmation: "",
-		type: "",
-	});
+// export default function Create() {
+// 	const [form, setForm] = useState({
+// 		// default state of account creation form
+// 		username: "",
+// 		password: "",
+// 		passwordConfirmation: "",
+// 		type: "",
+// 	});
 
-	function updateForm(value) {
-		return setForm((prev) => {
-			return { ...prev, ...value };
-		});
-	}
+// 	function updateForm(value) {
+// 		return setForm((prev) => {
+// 			return { ...prev, ...value };
+// 		});
+// 	}
 
-	async function onSubmit(e) {
-		e.preventDefault();
-        const newAccount = {...form};
+// 	async function onSubmit(e) {
+// 		//e.preventDefault();
+//         const newAccount = {...form};
+// 		console.log("In onSubmit");
+//         // const response = await fetch("http://localhost:4000/create", {
+//         //     method: "POST",
+//         //     headers: {
+//         //         "Content-Type": "application/json",
+//         //     },
+//         //     body: JSON.stringify(newAccount),
+//         // }).catch(error => {
+//         //     window.alert(error);
+//         //     return;
+//         // });
 
-        const response = await fetch("http://localhost:4000/create", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newAccount),
-        }).catch(error => {
-            window.alert(error);
-            return;
-        });
-
-        const account = await response.json();
+//         // const account = await response.json();
         
-        setForm({username: "", password: "", passwordConfirmation: "", type: ""});
+//         // setForm({username: "", password: "", passwordConfirmation: "", type: ""});
 
-        console.log("Success");
-	}
+//         // console.log("Success");
+// 	}
 
-	return (
-		<div>
-			<h2>Create Account</h2>
-			<form className="create-account" onSubmit={onSubmit}>
-				<div className="create-account-div">
-					<label htmlFor="username">Username</label>
-					<input
-						type="text"
-						className="input"
-						id="username"
-						value={form.username}
-						onChange={(e) => updateForm({ username: e.target.value })}
-					/>
-				</div>
-                <div className="create-account-div">
-                <label htmlFor="password">Password</label>
-					<input
-						type="text"
-						className="input"
-						id="password"
-						value={form.password}
-						onChange={(e) => updateForm({ password: e.target.value })}
-					/>
-                </div>
-                <div className="create-account-div">
-                <label htmlFor="passwordConfirmation">Confirm password</label>
-					<input
-						type="text"
-						className="input"
-						id="passwordConfirmation"
-						value={form.passwordConfirmation}
-						onChange={(e) => updateForm({ passwordConfirmation: e.target.value })}
-					/>
-                </div>
-                <div className="create-account-div">
-                    <p>Please choose an account type:  </p>
-                    <input type="radio" id="general" name="type" value="general"/>
-                    <label htmlFor="general">General</label>
-                    <input type="radio" id="business" name="type" value="business"/>
-                    <label htmlFor="business">Business</label>
-                    <input type="radio" id="student" name="type" value="student"/>
-                    <label htmlFor="student">Student</label>
-                    <input type="radio" id="travel" name="type" value="travel"/>
-                    <label htmlFor="travel">Travel</label>
-                </div>
-			</form>
-            <button type="submit" className="submit-button">Submit</button>
-            <div className="footer">
-                <p>Copyright &copy; 2024</p>
-            </div>
-		</div>
-	);
+// 	return (
+// 		<div>
+// 			<h2>Create Account</h2>
+// 			<form className="create-account" onSubmit={onSubmit}>
+// 				<div className="create-account-div">
+// 					<label htmlFor="username">Username</label>
+// 					<input
+// 						type="text"
+// 						className="input"
+// 						id="username"
+// 						value={form.username}
+// 						onChange={(e) => updateForm({ username: e.target.value })}
+// 					/>
+// 				</div>
+//                 <div className="create-account-div">
+//                 <label htmlFor="password">Password</label>
+// 					<input
+// 						type="text"
+// 						className="input"
+// 						id="password"
+// 						value={form.password}
+// 						onChange={(e) => updateForm({ password: e.target.value })}
+// 					/>
+//                 </div>
+//                 <div className="create-account-div">
+//                 <label htmlFor="passwordConfirmation">Confirm password</label>
+// 					<input
+// 						type="text"
+// 						className="input"
+// 						id="passwordConfirmation"
+// 						value={form.passwordConfirmation}
+// 						onChange={(e) => updateForm({ passwordConfirmation: e.target.value })}
+// 					/>
+//                 </div>
+//                 <div className="create-account-div">
+//                     <p>Please choose an account type:  </p>
+//                     <input type="radio" id="general" name="type" value="general" onChange={(e) => updateForm({ type: e.target.value})}/>
+//                     <label htmlFor="general">General</label>
+//                     <input type="radio" id="business" name="type" value="business" onChange={(e) => updateForm({ type: e.target.value})}/>
+//                     <label htmlFor="business">Business</label>
+//                     <input type="radio" id="student" name="type" value="student" onChange={(e) => updateForm({ type: e.target.value})}/>
+//                     <label htmlFor="student">Student</label>
+//                     <input type="radio" id="travel" name="type" value="travel" onChange={(e) => updateForm({ type: e.target.value})}/>
+//                     <label htmlFor="travel">Travel</label>
+//                 </div>
+// 			</form>
+//             {/* <button type="submit" className="submit-button" onSubmit={onSubmit}>Submit</button> */}
+//             <div className="footer">
+//                 <p>Copyright &copy; 2024</p>
+//             </div>
+// 		</div>
+// 	);
+// }
+
+
+import "./Create.css";
+import { useState } from "react";
+
+function Create () {
+    const [form, setForm] = useState({
+        username: "",
+        password: "",
+       // confirmPassword: "",
+        type: ""
+    });
+    const [invalidMessage, setInvalidMessage] = useState("");
+
+    const updateForm = (e) => {
+        setForm({...form, [e.target.name]:[e.target.value]})
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+       // let passwordCheck = 1;
+        console.log("In onSubmit");
+        console.log(form);
+
+        // check to make sure passwords match
+        // if (form.password.toString() === form.confirmPassword.toString()) {
+        //     passwordCheck = 0;
+        // } else {
+        //     console.log("Password error");
+        //     setInvalidMessage("Passwords must match");
+        //     passwordCheck = 1;
+        // }
+
+        // if(passwordCheck === 0) {
+
+        setForm({username: "", password: "", confirmPassword: "", type: ""});
+        //}
+    }
+
+    return (
+    <div className="container">
+        <h3>New Account</h3>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username" className="form-label">Username</label>
+            <input type="text" placeholder="Please enter username" 
+            onChange={(e) => updateForm(e)} required/>
+
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="text" placeholder="Please enter password" 
+            onChange={(e) => updateForm(e)} required/>
+        
+            {/* <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+            <input type="text" placeholder="Please confirm password" 
+            onChange={(e) => updateForm(e)} required/> */}
+
+            <label htmlFor="type" className="form-label">Account Type</label>
+            {/* <input type="radio" name="type"
+            onChange={(e) => updateForm(e)} /> Business
+            <input type="radio" name="type"
+            onChange={(e) => updateForm(e)} /> General
+            <input type="radio" name="type"
+            onChange={(e) => updateForm(e)} /> Student
+            <input type="radio" name="type"
+            onChange={(e) => updateForm(e)} /> Travel */}
+            <select name="type" id="type">
+                <option value="business">Business</option>
+                <option value="general">General</option>
+                <option value="student">Student</option>
+                <option value="travel">Travel</option>
+            </select>
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+        {invalidMessage}
+    </div>
+    );
 }
+
+export default Create;
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,23 @@
-export default function Login(){
-    return(
-        <div>Login</div>
-    )
+import "./Login.css";
+import React from "react";
+
+function Login () {
+    return (
+    <div className="container">
+        <h3>Login</h3>
+        <form>
+            <label htmlFor="username" className="form-label">Username</label>
+            <input type="text" placeholder="Please enter username" />
+
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="text" placeholder="Please enter password" />
+
+            <br />
+            
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+    );
 }
+
+export default Login;
